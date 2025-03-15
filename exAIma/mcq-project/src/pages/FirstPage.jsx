d537a6d16e8c2a1e11e7141ca67099b7dcb105e1
wokeii <137532823+stebi123@users.noreply.github.com>
2025-03-16 01:17:05 +0530

    Update FirstPage.jsx
    updated first page

--- exAIma/mcq-project/src/pages/FirstPage.jsx
+++ exAIma/mcq-project/src/pages/FirstPage.jsx
@@ -130,7 +130,7 @@ export default function ProctoringPage() {
             </svg>
             {/* </div> */}
           </motion.div>
-          <h1 className={`text-xl font-light ${darkMode ? 'text-green-400' : 'text-green-600'}`}>exAIma</h1>
+          <h1 className={`m-1 text-xl font-light ${darkMode ? 'text-green-400' : 'text-green-600'}`}>exAIma</h1>
         </motion.div>
         
         <div className="flex items-center space-x-4">
@@ -337,7 +337,7 @@ export default function ProctoringPage() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
             </svg>
             {/* </div> */}
-            <p className={`text-sm ${darkMode ? 'text-gray-400' : 'text-gray-500'}`}>
+            <p className={`m-1 text-sm ${darkMode ? 'text-gray-400' : 'text-gray-500'}`}>
               © {new Date().getFullYear()} exAIma. All rights reserved.
             </p>
           </div>
